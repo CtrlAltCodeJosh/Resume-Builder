ef3b70c711f7bacfb52fd97752df653699f2e15b
Joshua Reaves <joshua.j.reaves@gmail.com>
2017-12-17 11:01:11 -0800

    missed adding master window class

--- StatementsWindow.py
+++ StatementsWindow.py
@@ -1,5 +1,46 @@
 from Constant import *
+from WinMaster import ExtraWindowsMaster
 
-class PersonalStatementWindow:
-	def __init__(self):
-		None
+
+class PersonalStatementWindow(ExtraWindowsMaster):	
+	def __init__(self, caller, *args, **kwargs):
+		#inherit from the master of extra windows
+		ExtraWindowsMaster.__init__(self, caller)
+		
+		#set up variables and get data
+		self.statementButtons = []
+		username, name, phone, email, link, statements, jobs, eds, skillCat = self.User.getUserDetails()
+		
+		#set variables in master of extra windows (set top frame)
+		self.windowLabel.config(text = 'Add or edit your Personal Statements')
+		self.middleFrame.config(text = 'Edit Statements')
+		
+		#populate the midframe
+		self.populateMidFrame(statements)
+
+		#populate the bottom frame
+		self.addStatemntButton = Button(self.bottomFrame, width=GBUTTONWIDTH, text = 'Add new personal\nstatement', command = self.addStatement)
+		self.addStatemntButton.grid(row=0, column=0, padx=SMALLBPADDING, pady=SMALLBPADDING, sticky = W)
+		
+	def editStatement(self, statement):
+		print ("finding and editing statment")
+		
+	def addStatement(self):
+		
+		print ('adding new personal statement')
+		
+	def populateMidFrame(self, statements):
+		for item in self.middleFrame.winfo_children():
+			item.destroy()
+		if statements == []:
+			self.middleFrame.config(text = 'No statements have been set')
+		else:
+			count = 0 
+			for statement in statements:
+				statementButton = Button(self.middleFrame, text = statement, command = lambda: editStatement(statement))
+				statementButton.grid(row=count, column=0)
+				self.statementButtons.append(statementButton)
+				count = count + 1
+				
+				
+				
---
+++ WinMaster.py
@@ -0,0 +1,35 @@
+from Constant import *
+	
+class ExtraWindowsMaster:	
+	
+	def __init__(self, caller):
+		self.User = UserClasses.User()
+		self.master = Toplevel()
+		self.topFrame = Frame(self.master)#, width = GMASTERWIDTH/4, height=GMASTERHEIGHT/4)
+		self.middleFrame = LabelFrame(self.master, text = '', width = GMASTERWIDTH, height=GMASTERHEIGHT)
+		self.bottomFrame = Frame(self.master)#, width = GMASTERWIDTH/4, height=GMASTERHEIGHT/4)
+		self.topFrame.pack(side='top', fill = 'both', expand = True)
+		self.middleFrame.pack(side='top', fill = 'both', expand = True)
+		self.bottomFrame.pack(side='top', fill = 'both', expand = True)
+		self.returnButton = Button(self.bottomFrame, text = 'Main Window', command = self.closeAndUpdate)
+		self.returnButton.grid(row = 0, column = 99)
+		self.caller = caller
+		self.master.withdraw()
+		self.master.protocol("WM_DELETE_WINDOW", self.closeAndUpdate)
+		self.windowLabel = Label(self.topFrame, text = '', width=GBUTTONWIDTH,)
+		self.windowLabel.grid(row=0, column=0, padx=SMALLBPADDING, pady=SMALLBPADDING)
+		
+	def openWindow(self, User):
+		self.caller.buttonsOff()
+		self.master.deiconify()	
+		self.User = User
+		
+	def closeAndUpdate(self):
+		self.caller.buttonsOn()
+		self.caller.updateFrame(self.User)
+		self.master.withdraw()
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------				
+		
